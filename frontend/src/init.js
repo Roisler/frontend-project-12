50397bae9b59ef6862e234c9a9243c5dab5dd292
roisler <roislerxs@gmail.com>
2023-01-15 22:41:49 +0700

    add rollbarTest

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import i18n from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import App from './components/App';
 import store from './slices/index';
 import resources from './locales/index';
@@ -16,15 +17,30 @@ const init = async () => {
       lng: 'ru',
       fallbackLng: 'ru',
     });
+
+  const rollbarConfig = {
+    accessToken: 'da4bf007cb5041c6b3ca04c533ff7d42',
+    environment: 'testenv',
+  };
+  const TestError = () => {
+    const a = null;
+    return a.hello();
+  };
+
   leoProfanity.clearList();
   leoProfanity.add(leoProfanity.getDictionary('en'));
   leoProfanity.add(leoProfanity.getDictionary('ru'));
   const vdom = (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18nInstance}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <TestError />
+        <Provider store={store}>
+          <I18nextProvider i18n={i18nInstance}>
+            <App />
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
   return vdom;
 };
